8de3cca667ed189125381132b6bcdaf9e6e9329b
Collins <collowmartial@gmail.com>
2025-04-23 13:59:56 +0300

    modified text file

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,15 +1,15 @@
 import React from 'react';
 import MovieCard from './MovieCard';
 
-function MovieList( {
+function MovieList({
 
+  movies,
+  onToggleFavorite,
+  favorites,
+  onAddReview,
+  onUpdateLikes,
 }) {
-  const movies = [
-    { id: 1, title: 'Movie 1', year: 2021 },
-    { id: 2, title: 'Movie 2', year: 2020 },
-    { id: 3, title: 'Movie 3', year: 2019 },
-    // Add more movie objects as needed
-  ];
+  
 
   return (
     <div className="movie-list">
